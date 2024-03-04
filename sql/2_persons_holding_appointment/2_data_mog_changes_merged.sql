019c70a1dca67368e3656a42c0965390ed65f8dd
philipnye <philipnye@users.noreply.github.com>
2024-03-04 09:00:40 +0000

    Use post_relationship in row_number calc to make fully deterministic

--- sql/2_persons_holding_appointment/2_data_mog_changes_merged.sql
+++ sql/2_persons_holding_appointment/2_data_mog_changes_merged.sql
@@ -15,65 +15,76 @@ select
     max(end_date) "End date"
 from (
     select
-        case
-            when not ol1.id is null or not ol2.id is null then row_number() over (partition by ol1.id, ol2.id order by p.id, t.rank_equivalence, ac.cabinet_status, t.name)
-            else null
-        end row_number,
-        case
-            when ol1.id is null and ol2.id is null then random()
-            when ol1.id is null then ol2.id
-            when ol2.id is null then ol1.id
-        end link_id,
-        p.id_parliament,
-        p.name minister_name,
-        case
-            when r.house = 'Commons' then 'MP'
-             when r.house = 'Lords' then 'Peer'
-        end "MP/peer",
-        rc.party,
-        t.name post_name,
-        t.rank_equivalence,
-        o.short_name org_name,
-        ac.cabinet_status,
-        ac.is_on_leave,
-        ac.is_acting,
-        ac.leave_reason,
-        ac.start_date,
-        ac.end_date
-    from appointment a
-        inner join appointment_characteristics ac on
-            a.id = ac.appointment_id
-        inner join person p on
-            a.person_id = p.id and
-            coalesce(a.start_date, '1900-01-01') >= coalesce(p.start_date, '1900-01-01') and
-            coalesce(a.start_date, '1900-01-01') < coalesce(p.end_date, '9999-12-31')
-        left join representation r on
-            a.person_id = r.person_id and
-            coalesce(a.start_date, '1900-01-01') >= coalesce(r.start_date, '1900-01-01') and
-            coalesce(a.start_date, '1900-01-01') < coalesce(r.end_date, '9999-12-31')
-        left join representation_characteristics rc on
-            r.id = rc.representation_id and
-            coalesce(r.start_date, '1900-01-01') >= coalesce(rc.start_date, '1900-01-01') and
-            coalesce(r.start_date, '1900-01-01') < coalesce(rc.end_date, '9999-12-31')
-        inner join post t on
-            a.post_id = t.id
-        inner join organisation o on
-            t.organisation_id = o.id
-        left join organisation_link ol1 on
-            o.id = ol1.predecessor_organisation_id and
-            ac.end_date = ol1.link_date
-        left join organisation_link ol2 on
-            o.id = ol2.successor_organisation_id and
-            ac.start_date = ol2.link_date
-    where
-        t.id = @id and
-        coalesce(ac.end_date, '9999-12-31') > @date1 and
+        row_number() over (partition by appointment_characteristics_id order by continues_previous_appointment desc, group_name) row_number,
+        *
+    from (
+        select
+            case
+                when lag(ac.end_date) over (partition by pr.group_name order by ac.start_date asc) = ac.start_date then 1
+                else 0
+            end continues_previous_appointment,
+            pr.group_name,
+            case
+                when ol1.id is null and ol2.id is null then random()
+                when ol1.id is null then ol2.id
+                when ol2.id is null then ol1.id
+            end organisation_link_id,
+            p.id_parliament,
+            p.name minister_name,
+            case
+                when r.house = 'Commons' then 'MP'
+                when r.house = 'Lords' then 'Peer'
+            end "MP/peer",
+            rc.party,
+            t.name post_name,
+            t.rank_equivalence,
+            o.short_name org_name,
+            ac.id appointment_characteristics_id,
+            ac.cabinet_status,
+            ac.is_on_leave,
+            ac.is_acting,
+            ac.leave_reason,
+            ac.start_date,
+            ac.end_date
+        from appointment a
+            inner join appointment_characteristics ac on
+                a.id = ac.appointment_id
+            inner join person p on
+                a.person_id = p.id and
+                coalesce(a.start_date, '1900-01-01') >= coalesce(p.start_date, '1900-01-01') and
+                coalesce(a.start_date, '1900-01-01') < coalesce(p.end_date, '9999-12-31')
+            left join representation r on
+                a.person_id = r.person_id and
+                coalesce(a.start_date, '1900-01-01') >= coalesce(r.start_date, '1900-01-01') and
+                coalesce(a.start_date, '1900-01-01') < coalesce(r.end_date, '9999-12-31')
+            left join representation_characteristics rc on
+                r.id = rc.representation_id and
+                coalesce(r.start_date, '1900-01-01') >= coalesce(rc.start_date, '1900-01-01') and
+                coalesce(r.start_date, '1900-01-01') < coalesce(rc.end_date, '9999-12-31')
+            inner join post t on
+                a.post_id = t.id
+            inner join organisation o on
+                t.organisation_id = o.id
+            left join organisation_link ol1 on
+                o.id = ol1.predecessor_organisation_id and
+                ac.end_date = ol1.link_date
+            left join organisation_link ol2 on
+                o.id = ol2.successor_organisation_id and
+                ac.start_date = ol2.link_date
+            left join post_relationship pr on
+                pr.post_id = t.id
+        where
+            t.id = @id and
+            coalesce(ac.end_date, '9999-12-31') > @date1 and
             coalesce(ac.start_date, '1900-01-01') <= @date2
-    order by
-        coalesce(ac.start_date, '1900-01-01')
+        order by
+            coalesce(ac.start_date, '1900-01-01')
     ) q
+) q
+where
+    row_number = 1
 group by
-    link_id,
-    row_number
+    group_name,
+    organisation_link_id
 order by
     min(coalesce(start_date, '1900-01-01'))
