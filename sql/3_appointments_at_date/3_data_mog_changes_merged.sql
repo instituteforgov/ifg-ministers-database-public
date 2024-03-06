43371faf58322bb150cf7971b7f7e4df152483d8
philipnye <philipnye@users.noreply.github.com>
2024-03-06 17:18:04 +0000

    Add scripts

---
+++ sql/3_appointments_at_date/3_data_mog_changes_merged.sql
@@ -0,0 +1,89 @@
+select
+    min(id_parliament) id_parliament,
+    min(minister_name) Name,
+    min("MP/peer") "MP/peer",
+    min(party) Party,
+    case
+        when max(case when is_on_leave = 1 then 1 else 0 end) = 1 then group_concat(post_name || ' (on leave)', '/')
+        when max(case when is_acting = 1 then 1 else 0 end) = 1 then group_concat(post_name || ' (acting)', '/')
+        else group_concat(post_name, '/')
+    end Role,
+    group_concat(org_name, '/') Department,
+    min(rank_equivalence) Rank,
+    min(cabinet_status) "Cabinet status",
+    min(start_date) "Start date",
+    max(end_date) "End date"
+from (
+    select
+        row_number() over (partition by appointment_characteristics_id order by continues_previous_appointment desc, group_name) row_number,
+        *
+    from (
+        select
+            case
+                when lag(ac.end_date) over (partition by pr.group_name order by ac.start_date asc) = ac.start_date then 1
+                else 0
+            end continues_previous_appointment,
+            pr.group_name,
+            case
+                when ol1.id is null and ol2.id is null then random()
+                when ol1.id is null then ol2.id
+                when ol2.id is null then ol1.id
+            end organisation_link_id,
+            p.id_parliament,
+            p.name minister_name,
+            case
+                when r.house = 'Commons' then 'MP'
+                when r.house = 'Lords' then 'Peer'
+            end "MP/peer",
+            rc.party,
+            t.name post_name,
+            t.rank_equivalence,
+            o.short_name org_name,
+            ac.id appointment_characteristics_id,
+            ac.cabinet_status,
+            ac.is_on_leave,
+            ac.is_acting,
+            ac.leave_reason,
+            ac.start_date,
+            ac.end_date
+        from appointment a
+            inner join appointment_characteristics ac on
+                a.id = ac.appointment_id
+            inner join person p on
+                a.person_id = p.id and
+                @date >= coalesce(p.start_date, '1900-01-01') and
+                @date < coalesce(p.end_date, '9999-12-31')
+            left join representation r on
+                a.person_id = r.person_id and
+                @date >= coalesce(r.start_date, '1900-01-01') and
+                @date < coalesce(r.end_date, '9999-12-31')
+            left join representation_characteristics rc on
+                r.id = rc.representation_id and
+                @date >= coalesce(rc.start_date, '1900-01-01') and
+                @date < coalesce(rc.end_date, '9999-12-31')
+            inner join post t on
+                a.post_id = t.id
+            inner join organisation o on
+                t.organisation_id = o.id
+            left join organisation_link ol1 on
+                o.id = ol1.predecessor_organisation_id and
+                ac.end_date = ol1.link_date
+            left join organisation_link ol2 on
+                o.id = ol2.successor_organisation_id and
+                ac.start_date = ol2.link_date
+            left join post_relationship pr on
+                pr.post_id = t.id
+        order by
+            coalesce(ac.start_date, '1900-01-01')
+    ) q
+) q
+where
+    row_number = 1
+group by
+    group_name,
+    organisation_link_id
+having
+    @date >= min(start_date) and
+    @date < max(end_date)
+order by
+    min(minister_name)
---
+++ sql/4_appointments_exits_between_dates/4_data_mog_changes_merged.sql
@@ -0,0 +1,144 @@
+select
+    q.type "Appointment/exit",
+    q.date Date,
+    q.id_parliament,
+    q.minister_name Name,
+    case
+        when q.house = 'Commons' then 'MP'
+        when q.house = 'Lords' then 'Peer'
+    end "MP/peer",
+    q.party Party,
+    case
+        when q.is_on_leave = 1 then q.post_name || ' (on leave)'
+        when q.is_acting = 1 then q.post_name || ' (acting)'
+        else q.post_name
+    end Role,
+    q.org_name Department,
+    q.rank_equivalence Rank,
+    q.cabinet_status "Cabinet status"
+from (
+    select
+        'Appointment' type,
+        ac1.start_date date,
+        p.id_parliament,
+        p.name minister_name,
+        r.house,
+        rc.party,
+        t1.name post_name,
+        t1.rank_equivalence,
+        o1.short_name org_name,
+        ac1.cabinet_status,
+        ac1.is_on_leave,
+        ac1.is_acting,
+        ac1.leave_reason
+    from appointment a1
+        inner join appointment_characteristics ac1 on
+            a1.id = ac1.appointment_id and
+            @date1 <= coalesce(ac1.start_date, '1900-01-01') and
+            @date2 >= coalesce(ac1.start_date, '1900-01-01')
+        inner join person p on
+            a1.person_id = p.id and
+            coalesce(a1.start_date, '1900-01-01') >= coalesce(p.start_date, '1900-01-01') and
+            coalesce(a1.start_date, '1900-01-01') < coalesce(p.end_date, '9999-12-31')
+        left join representation r on
+            a1.person_id = r.person_id and
+            coalesce(a1.start_date, '1900-01-01') >= coalesce(r.start_date, '1900-01-01') and
+            coalesce(a1.start_date, '1900-01-01') < coalesce(r.end_date, '9999-12-31')
+        left join representation_characteristics rc on
+            r.id = rc.representation_id and
+            coalesce(a1.start_date, '1900-01-01') >= coalesce(rc.start_date, '1900-01-01') and
+            coalesce(a1.start_date, '1900-01-01') < coalesce(rc.end_date, '9999-12-31')
+        inner join post t1 on
+            a1.post_id = t1.id
+        inner join organisation o1 on
+            t1.organisation_id = o1.id
+    where
+        not exists (
+            select *
+            from appointment a2
+                inner join appointment_characteristics ac2 on
+                    a2.id = ac2.appointment_id
+                inner join post t2 on
+                    a2.post_id = t2.id
+                inner join organisation o2 on
+                    t2.organisation_id = o2.id
+                inner join organisation_link ol on
+                    o1.id = ol.successor_organisation_id
+                inner join post_relationship pr1 on
+                    t1.id = pr1.post_id
+                inner join post_relationship pr2 on
+                    t2.id = pr2.post_id
+            where
+                a1.person_id = a2.person_id and
+                ac1.start_date = ac2.end_date and
+                ac1.start_date = ol.link_date and
+                ol.predecessor_organisation_id = o2.id and
+                pr1.group_name = pr2.group_name
+            )
+
+    union all
+
+    select
+        'Exit' type,
+        ac1.end_date date,
+        p.id_parliament,
+        p.name minister_name,
+        r.house,
+        rc.party,
+        t1.name post_name,
+        t1.rank_equivalence,
+        o1.short_name org_name,
+        ac1.cabinet_status,
+        ac1.is_on_leave,
+        ac1.is_acting,
+        ac1.leave_reason
+    from appointment a1
+        inner join appointment_characteristics ac1 on
+            a1.id = ac1.appointment_id and
+            @date1 <= coalesce(ac1.end_date, '9999-12-31') and
+            @date2 >= coalesce(ac1.end_date, '9999-12-31')
+        inner join person p on
+            a1.person_id = p.id and
+            coalesce(a1.end_date, '9999-12-31') > coalesce(p.start_date, '1900-01-01') and
+            coalesce(a1.end_date, '9999-12-31') <= coalesce(p.end_date, '9999-12-31')
+        left join representation r on
+            a1.person_id = r.person_id and
+            coalesce(a1.end_date, '9999-12-31') > coalesce(r.start_date, '1900-01-01') and
+            coalesce(a1.end_date, '9999-12-31') <= coalesce(r.end_date, '9999-12-31')
+        left join representation_characteristics rc on
+            r.id = rc.representation_id and
+            coalesce(a1.end_date, '9999-12-31') > coalesce(rc.start_date, '1900-01-01') and
+            coalesce(a1.end_date, '9999-12-31') <= coalesce(rc.end_date, '9999-12-31')
+        inner join post t1 on
+            a1.post_id = t1.id
+        inner join organisation o1 on
+            t1.organisation_id = o1.id
+    where
+        not exists (
+            select *
+            from appointment a2
+                inner join appointment_characteristics ac2 on
+                    a2.id = ac2.appointment_id
+                inner join post t2 on
+                    a2.post_id = t2.id
+                inner join organisation o2 on
+                    t2.organisation_id = o2.id
+                inner join organisation_link ol on
+                    o1.id = ol.predecessor_organisation_id
+                inner join post_relationship pr1 on
+                    t1.id = pr1.post_id
+                inner join post_relationship pr2 on
+                    t2.id = pr2.post_id
+            where
+                a1.person_id = a2.person_id and
+                ac1.end_date = ac2.start_date and
+                ac1.end_date = ol.link_date and
+                ol.successor_organisation_id = o2.id and
+                pr1.group_name = pr2.group_name
+            )
+) q
+order by
+    q.date,
+    q.type desc,
+    q.minister_name
+
