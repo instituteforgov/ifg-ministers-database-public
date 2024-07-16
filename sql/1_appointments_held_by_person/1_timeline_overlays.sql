8f67ec034109c40403008672602dd101301cc999
philipnye <philipnye@users.noreply.github.com>
2024-07-16 17:35:32 +0100

    Hide subsequent 'General election' labels after first

--- sql/1_appointments_held_by_person/1_timeline_overlays.sql
+++ sql/1_appointments_held_by_person/1_timeline_overlays.sql
@@ -1,4 +1,7 @@
-SELECT *
+SELECT
+    q.label,
+    q.date,
+    q.persist
 FROM (
     SELECT
         'First became MP' label,
@@ -22,34 +25,78 @@ FROM (
 
     UNION
 
-    SELECT
-        e.display_name label,
-        e.date,
-        0 persist
-    FROM event e
-    WHERE
-        type = 'General election' AND
-        e.date > (
-            SELECT
-                MIN(start_date)
-            FROM representation r
-            WHERE
-                r.person_id IN (@minister_ids)
-        ) AND
-        e.date <= (
-            SELECT
-                CASE
-                    WHEN MAX(COALESCE(ac.end_date, '9999-12-31')) = '9999-12-31' THEN DATE('now')
-                    ELSE MAX(COALESCE(ac.end_date, '9999-12-31'))
-                END
-            FROM appointment a
-                INNER JOIN appointment_characteristics ac ON
-                    a.id = ac.appointment_id
-            WHERE
-                a.person_id IN (@minister_ids)
-        )
+    SELECT *
+    FROM (
+        SELECT
+            'General election' label,
+            e.date,
+            0 persist
+        FROM event e
+        WHERE
+            e.TYPE = 'General election' AND
+            e.date > (
+                SELECT
+                    MIN(start_date)
+                FROM representation r
+                WHERE
+                    r.person_id IN (@minister_ids)
+            ) AND
+            e.date <= (
+                SELECT
+                    CASE
+                        WHEN MAX(COALESCE(ac.end_date, '9999-12-31')) = '9999-12-31' THEN DATE('now')
+                        ELSE MAX(COALESCE(ac.end_date, '9999-12-31'))
+                    END
+                FROM appointment a
+                    INNER JOIN appointment_characteristics ac ON
+                        a.id = ac.appointment_id
+                WHERE
+                    a.person_id IN (@minister_ids)
+            )
+        ORDER BY
+            e.date
+        LIMIT 1
+    ) q
+
+    UNION
+
+    SELECT *
+    FROM (
+        SELECT
+            '' label,
+            e.date,
+            0 persist
+        FROM event e
+        WHERE
+            e.type = 'General election' AND
+            e.date > (
+                SELECT
+                    MIN(start_date)
+                FROM representation r
+                WHERE
+                    r.person_id IN (@minister_ids)
+            ) AND
+            e.date <= (
+                SELECT
+                    CASE
+                        WHEN MAX(COALESCE(ac.end_date, '9999-12-31')) = '9999-12-31' THEN DATE('now')
+                        ELSE MAX(COALESCE(ac.end_date, '9999-12-31'))
+                    END
+                FROM appointment a
+                    INNER JOIN appointment_characteristics ac ON
+                        a.id = ac.appointment_id
+                WHERE
+                    a.person_id IN (@minister_ids)
+            )
+        ORDER BY
+            e.date
+        LIMIT
+            -1
+        OFFSET
+            1
+    ) q
 ) AS q
 WHERE
-    date IS NOT NULL
+    q.date IS NOT NULL
 ORDER BY
-    date
+    q.date
