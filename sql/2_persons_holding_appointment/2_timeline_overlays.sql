bc77da3fbc7868ca7574acd504ca3a45338ef387
philipnye <philipnye@users.noreply.github.com>
2024-07-16 17:49:42 +0100

    Hide subsequent 'General election' labels after first

--- sql/2_persons_holding_appointment/2_timeline_overlays.sql
+++ sql/2_persons_holding_appointment/2_timeline_overlays.sql
@@ -1,11 +1,39 @@
-SELECT
-    e.display_name label,
-    e.date,
-    0 persist
-FROM event e
-WHERE
-    type = 'General election' AND
-    e.date >= @start_date AND
-    e.date <= @end_date
+SELECT *
+FROM (
+    SELECT
+        'General election' label,
+        e.date,
+        0 persist
+    FROM event e
+    WHERE
+        e.type = 'General election' AND
+        e.date >= @start_date AND
+        e.date <= @end_date
+    ORDER BY
+        e.date
+    LIMIT
+        1
+) q
+
+UNION
+
+SELECT *
+FROM (
+    SELECT
+        '' label,
+        e.date,
+        0 persist
+    FROM event e
+    WHERE
+        e.type = 'General election' AND
+        e.date >= @start_date AND
+        e.date <= @end_date
+    ORDER BY
+        e.date
+    LIMIT
+        -1
+    OFFSET
+        1
+) q
 ORDER BY
-    date
+    Q.DATE
