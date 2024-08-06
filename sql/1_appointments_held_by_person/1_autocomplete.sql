12cffd96d7db6803279c6aea7eca7b5e5d72c777
Graham May <graham.may@soapbox.co.uk>
2024-08-06 14:02:02 +0100

    Adding normalized_name where condition

--- sql/1_appointments_held_by_person/1_autocomplete.sql
+++ sql/1_appointments_held_by_person/1_autocomplete.sql
@@ -7,13 +7,25 @@ WHERE
     # "John Smith" will be broken up into individual words to return fuzzy matched results for the autocomplete.
     (
         # @name1 = `%John%`
-        p.display_name LIKE @name1
+        (
+            p.display_name LIKE @name1
+            OR
+            p.normalized_name LIKE @name1
+        )
         AND
         # @name2 = `%Smith%`
-        p.display_name LIKE @name2
+        (
+            p.display_name LIKE @name2
+            OR
+            p.normalized_name LIKE @name2
+        )
         AND
         # @name3,4,5 = `%Multiple_Names_here%`
-        p.display_name LIKE @name3
+        (
+            p.display_name LIKE @name3
+            OR
+            p.normalized_name LIKE @name3
+        )
     )
 ORDER BY
     p.display_name ASC
