5aba19798acbaab89bb566658e1744937ff1b1d8
philipnye <philipnye@users.noreply.github.com>
2024-08-07 18:08:56 +0100

    Fix SQL commenting

--- sql/2_persons_holding_appointment/2_autocomplete.sql
+++ sql/2_persons_holding_appointment/2_autocomplete.sql
@@ -2,15 +2,15 @@ SELECT
     t.display_name
 FROM post t
 WHERE
-    # "Prime Minister" will be broken up into individual words to return fuzzy matched results for the autocomplete.
+    -- "Prime Minister" will be broken up into individual words to return fuzzy matched results for the autocomplete.
     (
-        # @role1 = `%Prime%`
+        -- @role1 = `%Prime%`
         t.display_name LIKE @role1
         AND
-        # @role2 = `%Minister%`
+        -- @role2 = `%Minister%`
         t.display_name LIKE @role2
         AND
-        # @role3,4,5 = `%Multiple_Words_Here%`
+        -- @role3,4,5 = `%Multiple_Words_Here%`
         t.display_name LIKE @role3
     )
 GROUP BY
