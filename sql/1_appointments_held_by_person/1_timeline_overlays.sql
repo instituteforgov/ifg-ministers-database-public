4316f4b62e5450665d291842c804506cd4fce2c7
philipnye <philipnye@users.noreply.github.com>
2024-08-18 21:27:42 +0100

    Limit overlays to >=1974-05-04

--- sql/1_appointments_held_by_person/1_timeline_overlays.sql
+++ sql/1_appointments_held_by_person/1_timeline_overlays.sql
@@ -22,81 +22,79 @@ FROM (
     WHERE
         r.person_id IN (@minister_ids) AND
         r.house = 'Lords'
-
-    UNION
-
-    SELECT *
-    FROM (
-        SELECT
-            'General election' label,
-            e.date,
-            0 persist
-        FROM event e
-        WHERE
-            e.TYPE = 'General election' AND
-            e.date > (
-                SELECT
-                    MIN(start_date)
-                FROM representation r
-                WHERE
-                    r.person_id IN (@minister_ids)
-            ) AND
-            e.date <= (
-                SELECT
-                    CASE
-                        WHEN MAX(COALESCE(ac.end_date, '9999-12-31')) = '9999-12-31' THEN DATE('now')
-                        ELSE MAX(COALESCE(ac.end_date, '9999-12-31'))
-                    END
-                FROM appointment a
-                    INNER JOIN appointment_characteristics ac ON
-                        a.id = ac.appointment_id
-                WHERE
-                    a.person_id IN (@minister_ids)
-            )
-        ORDER BY
-            e.date
-        LIMIT 1
-    ) q
-
-    UNION
-
-    SELECT *
-    FROM (
-        SELECT
-            '' label,
-            e.date,
-            0 persist
-        FROM event e
-        WHERE
-            e.type = 'General election' AND
-            e.date > (
-                SELECT
-                    MIN(start_date)
-                FROM representation r
-                WHERE
-                    r.person_id IN (@minister_ids)
-            ) AND
-            e.date <= (
-                SELECT
-                    CASE
-                        WHEN MAX(COALESCE(ac.end_date, '9999-12-31')) = '9999-12-31' THEN DATE('now')
-                        ELSE MAX(COALESCE(ac.end_date, '9999-12-31'))
-                    END
-                FROM appointment a
-                    INNER JOIN appointment_characteristics ac ON
-                        a.id = ac.appointment_id
-                WHERE
-                    a.person_id IN (@minister_ids)
-            )
-        ORDER BY
-            e.date
-        LIMIT
-            -1
-        OFFSET
-            1
-    ) q
-) AS q
+) q
 WHERE
-    q.date IS NOT NULL
+    q.date >= '1979-05-04' AND
+    EXISTS (
+        SELECT *
+        FROM appointment a
+        WHERE
+            a.person_id in (@minister_ids) AND
+            q.date < COALESCE(a.end_date, '9999-12-31')
+    )
+
+UNION
+
+SELECT *
+FROM (
+    SELECT
+        'General election' label,
+        e.date,
+        0 persist
+    FROM event e
+    WHERE
+        e.type = 'General election' AND
+        e.date >= '1979-05-04' AND
+        EXISTS (
+            SELECT *
+            FROM representation r
+            WHERE
+                r.person_id in (@minister_ids) AND
+                e.date > r.start_date
+        ) AND
+        EXISTS (
+            SELECT *
+            FROM appointment a
+            WHERE
+                a.person_id in (@minister_ids) AND
+                e.date < COALESCE(a.end_date, '9999-12-31')
+        )
+    LIMIT 1
+) q
+
+UNION
+
+SELECT *
+FROM
+(
+    SELECT
+        '' label,
+        e.date,
+        0 persist
+    FROM event e
+    WHERE
+        e.type = 'General election' AND
+        e.date >= '1979-05-04' AND
+        EXISTS (
+            SELECT *
+            FROM representation r
+            WHERE
+                r.person_id in (@minister_ids) AND
+                e.date > r.start_date
+        ) AND
+        EXISTS (
+            SELECT *
+            FROM appointment a
+            WHERE
+                a.person_id in (@minister_ids) AND
+                e.date < COALESCE(a.end_date, '9999-12-31')
+        )
+    ORDER BY
+        e.date
+    LIMIT -1
+    OFFSET
+        1
+
+) q
 ORDER BY
     q.date
